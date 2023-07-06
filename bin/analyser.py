218af5d381d7e564913312607f83f7cfbb56f33f
Alejandra Camelo Cruz <jaime.camelo.cruz@uni-potsdam.de>
2023-07-06 19:42:02 +0200

    adding existing analyser

---
+++ bin/analyser.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import argparse
+import pandas as pd
+import numpy as np
+from preprocessor import Preprocessor
+
+class Analyser:
+    def __init__(self) -> None:
+        
