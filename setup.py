5c87d107105057dbde672a4dd2115f841c88f0cc
Alejandra Camelo Cruz <jaime.camelo.cruz@uni-potsdam.de>
2023-07-21 09:46:36 +0200

    added first setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 20 20:07:59 2023
+
+@author: alejandracamelocruz
+"""
+
+from setuptools import setup
+
+setup(
+      name = 'global_inflation_analyser',
+      version = '0.1',
+      author = ['Kshitij Kar',
+                'Bruk Asrat Tsega',
+                'Leon Oparin',
+                'Alejandra Camelo Cruz'],
+      packages = ['global_inflation_analyser'],
+      install_requires=[])
